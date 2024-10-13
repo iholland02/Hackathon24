0f9dfc4d594d70a56796333417eb3f4c3948db2e
Hidayah Husain <hidayah@shusain.com>
2024-10-13 00:16:07 -0500

    points+fee btn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Function to change the background color of the circle
-function paint(color) {
-  const circle = document.getElementById('circleID');
-  circle.style.backgroundColor = color; // Set the background color properly
-}
 
 // Function to shake the image
 function shakeImage() {
@@ -15,4 +10,22 @@ function shakeImage() {
   setTimeout(() => {
       image.classList.remove('shake');
   }, 500); // Match the animation duration (0.5s)
+}
+
+
+// Initial health bar width in pixels (starting at 150px)
+let healthWidth = 150;
+
+// Function to increase the width of the health bar
+function increaseHealth() {
+    const healthBar = document.getElementById('healthBar');
+    const maxWidth = 300; // Maximum width of the health bar
+
+    // Increase the width by 30px (or any increment you want)
+    if (healthWidth < maxWidth) {
+      healthWidth += 50;
+      healthBar.style.width = healthWidth + 'px';
+  } else {
+      console.log('Health is already at maximum!');
+  }
 }
